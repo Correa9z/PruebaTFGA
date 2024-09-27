d30950838dd434c530ac2cf6c296614eb8e24df9
Correa9z <juanpablo89correa@gmail.com>
2024-09-27 16:27:33 -0500

    Cambio estructura base de datos

--- db.sql
+++ db.sql
@@ -1,27 +1,31 @@
+-- Creación de la base de datos
 CREATE DATABASE pruebaT;
 
-USE DATABASE pruebaT;
+-- Referencia/uso de la base de datos
+USE pruebaT;
 
-
+-- Creación de la tabla departamentos
 CREATE TABLE departamentos(
-    id int AUTO_INCREMENT,
-    nombre varchar(100) UNIQUE,
-    PRIMARY KEY(id),
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(100) UNIQUE,
+    PRIMARY KEY(id)
 );
 
+-- Creación de la tabla empleados
 CREATE TABLE empleados(
-    id int AUTO_INCREMENT,
-    nombre varchar(100),
-    identificacion varchar(100) UNIQUE,
-    departamento_id int,
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(100),
+    identificacion VARCHAR(100) UNIQUE,
+    departamento_id INT,
     PRIMARY KEY(id),
-    FOREIGN KEY(departamento_id) REFERENCES(departamentos(id))  
+    FOREIGN KEY(departamento_id) REFERENCES departamentos(id)  
 );
 
+-- Creación de la tabla proyectos
 CREATE TABLE proyectos(    
-    id int AUTO_INCREMENT,
-    nombre varchar(100) UNIQUE,
-    empleado_id int,
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(100) UNIQUE,
+    empleado_id INT,
     PRIMARY KEY(id),
-    FOREIGN KEY(departamento_id) REFERENCES(departamentos(id))  
+    FOREIGN KEY(empleado_id) REFERENCES empleados(id)  
 );
